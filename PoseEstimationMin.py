cc88694c216f980cc6b9713c142c6f3f12f8358e
melikedograr <melikedograr@gmail.com>
2022-12-22 02:47:06 +0300

    Initial commit

---
+++ PoseEstimationMin.py
@@ -0,0 +1,24 @@
+import cv2 as cv2
+import mediapipe as mp
+import time
+
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+
+cap = cv2.VideoCapture('videos/8.mp4')
+ptime = 0
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+    print(results)
+
+    ctime = time.time()
+    fps = 1/(ctime-ptime)
+    ptime = ctime
+
+    cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
