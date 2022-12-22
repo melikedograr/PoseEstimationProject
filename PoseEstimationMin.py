8777aa6b92035edc20c73fbcb37c3b1c3e335021
melikedograr <melikedograr@gmail.com>
2022-12-22 14:45:05 +0300

    ready to modelling

--- PoseEstimationMin.py
+++ PoseEstimationMin.py
@@ -3,16 +3,26 @@ import mediapipe as mp
 import time
 
 mpPose = mp.solutions.pose
+mpDraw = mp.solutions.drawing_utils
 pose = mpPose.Pose()
 
-cap = cv2.VideoCapture('videos/8.mp4')
+cap = cv2.VideoCapture('videos/video8.mp4')
 ptime = 0
 
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = pose.process(imgRGB)
-    print(results)
+    print(results.pose_landmarks)
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
+            cx, cy = int(lm.x * w), int(lm.y * h)
+
+            cv2.circle(img, (cx, cy), 3, (255, 0, 255), cv2.FILLED)
+
+
 
     ctime = time.time()
     fps = 1/(ctime-ptime)
